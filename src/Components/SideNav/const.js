458d00223ca53c8f917d5b1401a5f5ba025e4490
Varun Miyani <varun.miyani@o9solutions.com>
2023-09-20 16:00:40 +0530

    layout updated

---
+++ src/Components/SideNav/const.js
@@ -0,0 +1,34 @@
+export const ITEMS = [
+	{
+		text: 'Inbox',
+		icon: 'k-i-inbox',
+		selected: true,
+		route: '/',
+	},
+	{
+		separator: true,
+	},
+	{
+		text: 'Notifications',
+		icon: 'k-i-bell',
+		route: '/notifications',
+	},
+	{
+		text: 'Calendar',
+		icon: 'k-i-calendar',
+		route: '/calendar',
+	},
+	{
+		separator: true,
+	},
+	{
+		text: 'Attachments',
+		icon: 'k-i-hyperlink-email',
+		route: '/attachments',
+	},
+	{
+		text: 'Favourites',
+		icon: 'k-i-star-outline',
+		route: '/favourites',
+	},
+];
