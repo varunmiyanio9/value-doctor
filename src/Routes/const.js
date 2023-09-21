c0d3fe3be1d86eb29890cf49a1588fa957566b38
Varun Miyani <varun.miyani@o9solutions.com>
2023-09-21 12:10:37 +0530

    inputs page added

--- src/Routes/const.js
+++ src/Routes/const.js
@@ -1,6 +1,7 @@
 // Public Pages
 import Dashboard from '../pages/Dashboard';
 import Sow from '../pages/Sow';
+import Inputs from '../pages/Inputs';
 
 // Auth Protected Pages
 // N/A
@@ -8,11 +9,13 @@ import Sow from '../pages/Sow';
 export const NAV_ITEMS = [
 	{ path: '/dashboard', name: 'Dashboard' },
 	{ path: '/sow', name: 'Sow' },
+	{ path: '/sow', name: 'Sow' },
 ];
 
 export const publicRoutes = [
 	{ path: '/dashboard', component: Dashboard, name: 'Dashboard' },
 	{ path: '/sow', component: Sow, name: 'Sow' },
+	{ path: '/inputs', component: Inputs, name: 'Inputs' },
 ];
 
 export const authProtectedRoutes = [];
