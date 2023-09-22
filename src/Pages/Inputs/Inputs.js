325f22c9b4f2d60d315103ebddfbb875e01e15d7
Akshay Tiwari <akshay.tiwari@o9solutions.com>
2023-09-22 10:45:38 +0530

    added SOW page, related images, and styles

--- src/Pages/Inputs/Inputs.js
+++ src/Pages/Inputs/Inputs.js
@@ -4,6 +4,7 @@ import { Grid, GridColumn } from '@progress/kendo-react-grid';
 import { TabStrip, TabStripTab } from '@progress/kendo-react-layout';
 import SupplyPlanningFoundationData from './SupplyPlanningFoundation.json';
 import ReplenishmentData from './Replenishment.json';
+import ForecastData from './Forecast.json';
 
 const Inputs = () => {
 	const [selected, setSelected] = React.useState(1);
@@ -83,12 +84,78 @@ const Inputs = () => {
 			</TabStripTab>
 			<TabStripTab title='Replenishment Data'>
 				<Grid
-					editField='inEdit'
 					style={{
 						height: '100%',
 						width: '100%',
 					}}
 					data={ReplenishmentData}
+				>
+					<GridColumn field='SNO' title='SNO' width='40px' />
+					<GridColumn field='Data' title='Data' />
+					<GridColumn field='Custom' title='Custom' />
+					<GridColumn
+						field='Industry sprint plan'
+						title='Industry sprint plan'
+					/>
+					<GridColumn
+						field='Planned sprint release'
+						title='Planned sprint release'
+					/>
+					<GridColumn
+						field='Actual sprint release'
+						title='Actual sprint release'
+					/>
+					<GridColumn
+						field='Average Implementaion Time (weeks )'
+						title='Average Implementaion Time (weeks)'
+					/>
+					<GridColumn field='Complexity' title='Complexity' />
+					<GridColumn
+						field='Tag To Value( Drop Down)'
+						title='Tag To Value(Drop Down)'
+					/>
+					<GridColumn
+						field='Config/Integration/ Plugin'
+						title='Config/Integration/Plugin'
+					/>
+					<GridColumn
+						field='User Story part Of CR'
+						title='User Story part Of CR'
+					/>
+					<GridColumn
+						field='User story are approved by customer'
+						title='User story are approved by customer'
+					/>
+					<GridColumn
+						field='User story Part of Ref model'
+						title='User story Part of Ref model'
+					/>
+					<GridColumn field='Architect Comment' title='Architect Comment' />
+					<GridColumn field='Config Lead Comment' title='Config Lead Comment' />
+					<GridColumn
+						field='Integration Archtect Comment'
+						title='Integration Archtect Comment'
+					/>
+					<GridColumn
+						field='Client management comment'
+						title='Client management comment'
+					/>
+					<GridColumn field='Risk' title='Risk' />
+					<GridColumn field='ISL/ IDB Comment' title='ISL/ IDB Comment' />
+					<GridColumn field='Impact' title='Impact' />
+					<GridColumn
+						field='Scope For Performance Improvement'
+						title='Scope For Performance Improvement'
+					/>
+				</Grid>
+			</TabStripTab>
+			<TabStripTab title='Forecast Data'>
+				<Grid
+					style={{
+						height: '100%',
+						width: '100%',
+					}}
+					data={ForecastData}
 				>
 					<GridColumn field='SNO' title='SNO' width='40px' />
 					<GridColumn field='Data' title='Data' />
--- src/Pages/Sow/Sow.js
+++ src/Pages/Sow/Sow.js
@@ -1,9 +1,188 @@
 import React from 'react';
+import './sow.scss';
+import salesCycleImg from './img/sales-cycle.png';
+import marketingLead from './img/marketing-lead.svg';
+import keyImage from './img/key.svg';
+import deviationImage from './img/deviation.png';
+import promiseImage from './img/Promise.png';
+import partnerImage from './img/implementation-partner.png';
+import dollarImage from './img/dollar.png';
+import machineNetworkImage from './img/machine-network.png';
+import machineImage from './img/machine.png';
+import tabletImage from './img/tablet.png';
+import dollarSaveImage from './img/dollar-save.png';
+
+const sowValues = {
+	duration: '6 Months',
+	customerSegmentation: 'Elephant',
+	companyRevenue: '$ 1 Billion',
+	reference: 'NA',
+};
 
 const Sow = props => {
 	return (
 		<div>
-			<h1>Sow</h1>
+			<div className='segment'>
+				<div className='segment-left'>
+					<div className='sales-seg'>
+						<div className='sales-pic-section'>
+							<img
+								className='sales-pic-section-img'
+								src={salesCycleImg}
+								alt='sale cycle'
+							/>
+						</div>
+						<div className='sales-desc-section'>
+							<div>
+								<span className='sc-text'>Sales Cycles </span>
+							</div>
+							<div>
+								<span>Duration : </span>
+								<span className='scd-values'>{sowValues.duration}</span>
+							</div>
+							<div>
+								<span>Company Revenue : </span>
+								<span className='scd-values'>{sowValues.companyRevenue}</span>
+							</div>
+							<div>
+								<span>Reference : </span>
+								<span className='scd-values'>{sowValues.reference}</span>
+							</div>
+							<div>
+								<span>Contract : </span>
+								<span className='scd-values'>{sowValues.reference}</span>
+							</div>
+						</div>
+					</div>
+
+					<div className='marketing-seg'>
+						<div className='marketing-pic-section'>
+							<img
+								className='marketing-pic-section-img'
+								src={marketingLead}
+								alt='marketing lead'
+							/>
+						</div>
+						<div className='marketing-desc-section'>
+							<div>
+								<span>Marketing Lead </span>
+							</div>
+							<div>
+								<span>Customer Segmentation: </span>
+								<span>{sowValues.customerSegmentation}</span>
+							</div>
+						</div>
+					</div>
+
+					<div className='sow-seg'>
+						<div className='sow'>
+							<span>SOW</span>
+						</div>
+						<div className='sow-seg'>
+							<div className='light-green'>
+								<span>Tag-Supply planning Foundation </span>
+							</div>
+							<div className='light-green'>
+								<span>Tag-Production planning </span>
+							</div>
+							<div className='light-green'>
+								<span>Tag-Procurment planning </span>
+							</div>
+							<div className='light-yellow'>
+								<span>Tag- Custom - Order Promissing</span>
+							</div>
+							<div className='light-yellow'>
+								<span>Tag- Truck Load </span>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className='segment-right'>
+					<div className='key-seg'>
+						<div className='key-pic-seg'>
+							<img className='key-pic-seg-img' src={keyImage} alt='Key' />
+						</div>
+						<div className='key-desc-seg'>
+							<div className='sh'>
+								<span>Stake Holders </span>
+							</div>
+							<div>
+								<span>Users</span>
+							</div>
+							<div>
+								<span>Interests</span>
+							</div>
+							<div>
+								<span>Traction</span>
+							</div>
+						</div>
+					</div>
+
+					<div className='projections-seg'>
+						<div className='promise-seg'>
+							<div className='promise-image-seg'>
+								<img
+									className='promise-seg-img'
+									src={promiseImage}
+									alt='Promise'
+								/>
+								<div className='bold promise-text'> Promise</div>
+							</div>
+						</div>
+						<div className='propositions-seg'>
+							<img
+								className='proposition-images'
+								src={dollarImage}
+								alt='Dollars/ Monetary'
+							/>
+							<img
+								className='proposition-images'
+								src={machineNetworkImage}
+								alt='Machine Network'
+							/>
+							<img
+								className='proposition-images'
+								src={machineImage}
+								alt='Machine'
+							/>
+							<img
+								className='proposition-images'
+								src={tabletImage}
+								alt='tablet'
+							/>
+							<img
+								className='proposition-images'
+								src={dollarSaveImage}
+								alt='Approximate Value'
+							/>
+							<span>Approximate Value Proposition</span>
+						</div>
+						<div className='implemention-partner-seg'>
+							<div>
+								<img className='ips-img' src={partnerImage} alt='partners' />
+							</div>
+							<div> Implementation Partners</div>
+						</div>
+					</div>
+
+					<div className='deviation-seg'>
+						<div className='deviation-blocks'>
+							<img src={deviationImage} alt='deviation' />
+						</div>
+						<div className='deviation-blocks'>
+							<div className='deviation-text'>% deviation from core blocks</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* <div className='segment'>
+				<div className='segment-left'>This is sales cycle under segment 2</div>
+				<div className='segment-right'>
+					This is key stake holder under segment 2
+				</div>
+			</div> */}
 		</div>
 	);
 };
